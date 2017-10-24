9afc520a203b3f12571148441bf97b9665bed4e0
Steven <stevenadrian02@hotmail.com>
2017-10-24 03:03:46 -0400

    Cyclops HW

---
+++ Cyclops/Assets/Scripts/Laser.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    public GameObject sphere;
+    public GameObject prefab;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray beam = Camera.main.ScreenPointToRay(Input.mousePosition); //beam is taking point on curser
+
+        Debug.DrawRay(beam.origin, beam.direction * 100f);
+
+        RaycastHit beamHit = new RaycastHit();
+
+        if (Physics.Raycast(beam, out beamHit, 100f))
+        {
+            Debug.Log("You hit something");
+
+        }
+       
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
+            { //if ray cast is looking at something destroy it.
+                if (hit.collider.gameObject.tag == "Hit") { 
+
+                  Destroy(hit.collider.gameObject);
+                }
+            }
+
+        }
+    }
+}
---
+++ Cyclops/Assets/Scripts/Seek.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Seek : MonoBehaviour
+{
+
+    public Transform target;
+    Rigidbody seeker;
+    public float thrust;
+
+    // Use this for initialization
+    void Start()
+    {
+        seeker = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 targetDir = Vector3.Normalize(target.position - transform.position);
+        seeker.AddForce(targetDir * thrust);
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
+            { //if ray cast is looking at something destroy it.
+                if (hit.collider.gameObject.tag == "good")
+                {
+                    Debug.Log("You hit a friendly!");
+
+                    Destroy(GetComponent<Laser>());
+                }
+            }
+
+        }
+    }
+}
